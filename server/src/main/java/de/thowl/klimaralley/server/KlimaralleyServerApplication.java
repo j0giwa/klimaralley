7e83b5ab1c5baf25ef22a3df92ad225c08177ab3
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-04-11 19:44:45 +0200

    Added server template

---
+++ server/src/main/java/de/thowl/klimaralley/server/KlimaralleyServerApplication.java
@@ -0,0 +1,13 @@
+package de.thowl.klimaralley.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlimaralleyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlimaralleyServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/de/thowl/klimaralley/server/KlimaralleyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thowl.klimaralley.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlimaralleyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
