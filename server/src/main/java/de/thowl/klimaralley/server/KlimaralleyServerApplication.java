f725c342e85f72ee1722a922d1365d1f6f3ee089
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2024-04-11 19:52:36 +0200

    (Probably) Prevent Problems with the Security library

--- server/src/main/java/de/thowl/klimaralley/server/KlimaralleyServerApplication.java
+++ server/src/main/java/de/thowl/klimaralley/server/KlimaralleyServerApplication.java
@@ -3,7 +3,8 @@ package de.thowl.klimaralley.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication // Commented in case SecurityAutoConfiguration causes problems
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 public class KlimaralleyServerApplication {
 
 	public static void main(String[] args) {
